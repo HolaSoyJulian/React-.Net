ec318f3020f2cc438d95ea31ee7aa4b211418112
JulianP <julian.pena@wactechnology.com>
2024-06-13 13:47:48 -0500

    Initial commit

---
+++ Crud-API/Models/Objeto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Crud_API.Models
+{
+    [Table("objeto")]
+    public class Objeto
+    {
+        [Key]
+        public long id { get; set; }
+
+        public string nombre { get; set; }
+
+        public string descripcion { get; set; }
+
+        public double precio { get; set; }
+
+        public DateTime fecha_creacion { get; set; }
+    }
+}
